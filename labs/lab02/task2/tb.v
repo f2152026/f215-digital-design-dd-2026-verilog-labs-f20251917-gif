ef16b1a0f4ec8bc70b4411e5d731acf6ffade89a
f20251917-gif <f20251917@pilani.bits-pilani.ac.in>
2026-09-19 17:06:51 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,27 +1,61 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
+// Testbench for parameterized LUT
 
 module tb;
 
-  // TODO: declare the inputs and outputs
+    // DEPTH = 8 requires 3 address bits
+    reg [2:0] t_sel;
+    wire [7:0] t_dout;
 
-  // TODO: instantiate DUT here
+    // Instantiate LUT with parameter override
+    lut #(
+        .WIDTH(8),
+        .DEPTH(8)
+    ) U1 (
+        .sel(t_sel),
+        .dout(t_dout)
+    );
 
-  // Waveform dump configuration (DO NOT CHANGE)
-  string vcd_file;
-  initial begin
-    if ($value$plusargs("vcd=%s", vcd_file)) begin
-      $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+    // Waveform dump configuration (DO NOT CHANGE)
+    string vcd_file;
+
+    initial begin
+        if ($value$plusargs("vcd=%s", vcd_file)) begin
+            $dumpfile(vcd_file);
+            $dumpvars(0, U1);
+        end
     end
-  end
 
-  initial begin
-    // TODO: apply different input combinations
+    // Test every valid address
+    initial begin
+        t_sel = 0;
+        #5;
 
-  end
+        t_sel = 1;
+        #5;
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+        t_sel = 2;
+        #5;
+
+        t_sel = 3;
+        #5;
+
+        t_sel = 4;
+        #5;
+
+        t_sel = 5;
+        #5;
+
+        t_sel = 6;
+        #5;
+
+        t_sel = 7;
+        #5;
+
+        $finish;
+    end
+
+    initial
+        $monitor($time, " sel=%d | dout=%d", t_sel, t_dout);
 
 endmodule
